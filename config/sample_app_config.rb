279cb9ecdccd01998597fcfee51d3589160ef8bb
Denis R <denis.rhoden@gmail.com>
2012-11-20 21:01:11 -0500

    settings

--- config/sample_app_config.rb
+++ config/sample_app_config.rb
@@ -5,6 +5,8 @@ class SampleAppConfig < Sinatra::Base
 
   configure :development, :test do
     enable :sessions, :logging, :static, :inline_templates, :method_override, :dump_errors, :run
+    set :app_file, File.join('./application.rb')
+    set :views, settings.root + '/views'
     Mongoid.load!(File.expand_path(File.join("config", "mongoid.yml")))
   end
 
